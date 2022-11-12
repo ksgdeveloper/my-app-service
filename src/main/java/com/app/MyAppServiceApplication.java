198555b134e1ad2830152a3b7c24d2ef39b9609b
ksgdeveloper <ksg.developer@gmail.com>
2022-11-12 09:12:33 +0530

    Adding Project folder to the commit message

---
+++ src/main/java/com/app/MyAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppServiceApplication.class, args);
+	}
+
+}
